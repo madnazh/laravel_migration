dccc7edf4b92e15e7ecaeabf72162ec1cc68b1e6
naufalazh <naufalazhari5@gmail.com>
2020-08-07 11:38:49 +0800

    first commit

---
+++ database/migrations/2020_08_06_132402_create_jawaban_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('isi', 255);
+            $table->timestamp('tanggal_dibuat');
+            $table->timestamp('tanggal_diperbaharui')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawaban');
+    }
+}
